312b649f21dcaf46c96ac1670b3c6d9628418f46
Dinithipurna <dinithi@ahlab.org>
2025-03-16 02:39:06 +0800

    add projects

--- old/script.js
+++ old/script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+function toggleProject(element) {
+    const content = element.querySelector('.project-content');
+
+    if (element.classList.contains("expanded")) {
+        content.style.maxHeight = "0";
+        content.style.paddingTop = "0";
+        element.classList.remove("expanded");
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px"; // Expand to fit content
+        content.style.paddingTop = "10px";
+        element.classList.add("expanded");
+    }
+}
